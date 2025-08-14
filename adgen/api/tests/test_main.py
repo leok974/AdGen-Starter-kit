6d565568593a89741ee699dfde755bf2b3e2845e
leok974 <leoklemet.pa@gmail.com>
2025-08-14 19:13:23 -0400

    fixed some stuff

--- adgen/api/tests/test_main.py
+++ adgen/api/tests/test_main.py
@@ -3,10 +3,12 @@ from adgen.api.main import app
 
 client = TestClient(app)
 
+
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
 
 def test_read_health():
     response = client.get("/health")
